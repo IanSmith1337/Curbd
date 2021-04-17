a6999bd0c77cd95cfc9ce4aa9ab6c7c807dcb1c6
Trian <iansmith1337@gmail.com>
2021-04-17 18:08:45 -0400

    redefine vars

--- public/signout.js
+++ public/signout.js
@@ -1,29 +1,30 @@
 document.onload = function loaded(){
-    var e = document.body.createElement("h2");
-    e.textContent = "Signing out...";
-    e.style.color = "black";
-    document.body.appendChild(e);
-    firebase.auth().signOut().then(() => {
-      setTimeout(function(){checkLogout();}, 5000);
-    }).catch((err) => {
-      e.textContent = "An error occured trying to sign you out. Try again later.";
-      e.style.color = "red";
-      document.body.replaceChild(e, old);
-      console.log(err.message);
-    });
+  var e = document.body.createElement("h2");
+  e.textContent = "Signing out...";
+  e.style.color = "black";
+  document.body.appendChild(e);
+  var old = document.body.childNodes[0];
+  firebase.auth().signOut().then(() => {
+    setTimeout(function(){checkLogout();}, 5000);
+  }).catch((err) => {
+    e.textContent = "An error occured trying to sign you out. Try again later.";
+    e.style.color = "red";
+    document.body.replaceChild(e, old);
+    console.log(err.message);
+  });
     
-    function checkLogout(){
-      if(firebase.auth().currentUser == null) {
-        e.textContent = "Sign out successful. Redirecting you back to the main site...";
-        e.style.color = "black";
-        var old = document.body.childNodes[0];
-        document.body.replaceChild(e, old);
-        setTimeout(function(){window.location.href = "https://curbid.web.app"}, 5000);
-      } else {
+  function checkLogout(){
+    if(firebase.auth().currentUser == null) {
+      e.textContent = "Sign out successful. Redirecting you back to the main site...";
+      e.style.color = "black";
+      old = document.body.childNodes[0];
+      document.body.replaceChild(e, old);
+      setTimeout(function(){window.location.href = "https://curbid.web.app"}, 5000);
+    } else {
         e.textContent("Sign out unsuccessful. Try again in a few minutes.");
         e.style.color = "red";
-        var old = document.body.childNodes[0];
+        old = document.body.childNodes[0];
         document.body.replaceChild(e, old);
       }
     }
-  }
+}
