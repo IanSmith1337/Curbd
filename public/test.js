e08ffc22d0b54908497fefe3250d0bceef938fd7
Trian <iansmith1337@gmail.com>
2021-04-24 16:33:46 -0400

    add first two tests

--- public/test.js
+++ public/test.js
@@ -7,7 +7,7 @@ firebase.auth().onAuthStateChanged(function (user) {
             var admin = doc.data().admin;
             var timeSinceLastLog = new Date(doc.data().lastLogin);
             var currentTime = new Date;
-            var differenceInMins = ((timeSinceLastLog.getTime() - currentTime.getTime())/60000)
+            var differenceInMins = (Math.abs((timeSinceLastLog.getTime() - currentTime.getTime())/60000))
             console.log(differenceInMins);
             console.log(differenceInMins < 20);
             if(admin || differenceInMins < 20) {
@@ -17,6 +17,13 @@ firebase.auth().onAuthStateChanged(function (user) {
                     test = document.getElementById("t" + i);
                     test.value = "Ready."
                 }
+                document.getElementById("start").addEventListener("click", function () {
+                    test1(user);
+                    test2("d7iFqf89EqbdSPlfb4s1M1N8Pbq2");
+                    test4();
+                    test5();
+                    test6();
+                });
             } else {
                 console.log("admin status red");
                 window.location.href = "https://curbid.web.app/testLogin.html";
@@ -27,3 +34,28 @@ firebase.auth().onAuthStateChanged(function (user) {
         window.location.href = "https://curbid.web.app/testLogin.html";
     }
 });
+
+function test1(user) {
+    var result = document.getElementById("t1");
+    if(user != null) {
+        result.className = "col-12 font-italic font-weight-bold text-center text-success"
+        result.innerHTML = "PASSED"
+    } else {
+        result.className = "col-12 font-italic font-weight-bold text-center text-danger"
+        result.innerHTML = "FAILED"
+    }
+}
+
+function test2(uid) {
+    var result = document.getElementById("t1");
+    db.collection("users").doc(uid).update({
+        tel: "123-456-7890",
+        address: "224 Main St., Towson, MD, 21252"
+    }).then(() => {
+        result.className = "col-12 font-italic font-weight-bold text-center text-success"
+        result.innerHTML = "PASSED"
+    }).catch((error) => {
+        result.className = "col-12 font-italic font-weight-bold text-center text-danger"
+        result.innerHTML = "FAILED"
+    })
+}
