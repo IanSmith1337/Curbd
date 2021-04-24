f29c0ef3a2d18904c1ad5553edc1d105e437b1a4
Trian <iansmith1337@gmail.com>
2021-04-24 00:09:17 -0400

    Moving functions around

--- public/test.js
+++ public/test.js
@@ -25,30 +25,30 @@ function resignIn(email, password) {
             throw new Error("Password cannot be empty.")
         }
         firebase.auth().signInWithEmailAndPassword(email, password).then((userCredential) => {
-            credential = userCredential.user;
+            credential = userCredential;
             db.collection("users").doc(user.uid).update({
                 signin: true,
                 lastLogin: date.toUTCString()
             }).then(() => {
                 console.log("Document updated successfully.");
             });
-            user.reauthenticateWithCredential(credential).then(function () {
-                var admin;
-                db.collection("users").doc(user.uid).get().then((doc) => {
-                    admin = doc.data().isAdmin;
-                });
-                if (admin) {
-                    var a = new a();
-                } else {
-                    throw Error();
-                }
-            }).catch(function (error) {
-                document.body.innerHTML = ""
-                document.body.appendChild(document.createElement("h1").textContent("Access denied."));
-            });
         }).catch((error) => {
             e.textContent = e.textContent + error.message;
             document.body.appendChild(e);
+        });
+        user.reauthenticateWithCredential(credential).then(function () {
+            var admin;
+            db.collection("users").doc(user.uid).get().then((doc) => {
+                admin = doc.data().isAdmin;
+            });
+            if (admin) {
+                var a = new a();
+            } else {
+                throw Error();
+            }
+        }).catch(function (error) {
+            document.body.innerHTML = ""
+            document.body.appendChild(document.createElement("h1").textContent("Access denied."));
         });
     } catch (error) {
         e.textContent = e.textContent + error.message;
