76cef0a70b1f239f7a53580ce33f89f9e16670ab
Trian <iansmith1337@gmail.com>
2021-04-24 15:21:36 -0400

    add logging

--- public/test.js
+++ public/test.js
@@ -11,10 +11,12 @@ firebase.auth().onAuthStateChanged(function (user) {
                 }
             
             } else {
+                console.log("admin status red");
                 window.location.href = "https://curbid.web.app/testLogin.html";
             }
         });
     } else {
+        console.log("refer/null red");
         window.location.href = "https://curbid.web.app/testLogin.html";
     }
 });
