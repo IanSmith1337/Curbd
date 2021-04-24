ce8aee4e01d402d0d26609ee751b001e0ea91ddb
Trian <iansmith1337@gmail.com>
2021-04-24 15:18:30 -0400

    Update admin for test.js

--- public/test.js
+++ public/test.js
@@ -1,20 +1,19 @@
 firebase.auth().onAuthStateChanged(function (user) {
     if (user != null && document.referrer != "https://curbid.web.app/testLogin.html") {
-        var admin = false;
         db.collection("users").doc(user.uid).get().then((doc) => {
-            admin = doc.data().admin;
-        });
-        if(admin) {
-            var test = document.getElementById("t1");
-            test.value = "Ready."
-            for(var i = 0; i < 5; i++){
-                test = document.getElementById("t" + i);
+            var admin = doc.data().admin;
+            if(admin) {
+                var test = document.getElementById("t1");
                 test.value = "Ready."
+                for(var i = 0; i < 5; i++){
+                    test = document.getElementById("t" + i);
+                    test.value = "Ready."
+                }
+            
+            } else {
+                window.location.href = "https://curbid.web.app/testLogin.html";
             }
-        
-        } else {
-            window.location.href = "https://curbid.web.app/testLogin.html";
-        }
+        });
     } else {
         window.location.href = "https://curbid.web.app/testLogin.html";
     }
