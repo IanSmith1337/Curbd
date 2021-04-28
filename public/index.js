20c422ba9cb7559fdfe71c44eef2e69ebf58d1ee
Trian <iansmith1337@gmail.com>
2021-04-27 21:22:28 -0400

    edit text and xhr

--- public/index.js
+++ public/index.js
@@ -118,7 +118,9 @@ function updatePostcards() {
       item.alt = alt;
     }
     xhr.open('GET', image);
-    setTimeout(function () {xhr.send()}, 5000);
+    setTimeout(function () {
+      xhr.send()
+    }, 5000);
   }
 
   function addText(item, text) {
@@ -135,73 +137,85 @@ function updatePostcards() {
 
   db.collection("posts").limit(50).onSnapshot((querySnapshot) => {
     //setTimeout(function () {
-      var cardRoot = createItem("div");
-      var main = document.getElementById("main");
-      if (document.getElementsByClassName("card-group row").length === 0) {
-        addClass(cardRoot, "card-group row");
-        append(cardRoot, main);
-      } else {
-        document.getElementsByClassName("card-group row").item(0).remove();
-        addClass(cardRoot, "card-group row");
-        append(cardRoot, main);
-      }
-      querySnapshot.forEach((doc) => {
-        if (!doc.data().hide && !doc.metadata.hasPendingWrites) {
-          var cardWrapper = createItem("div");
-          addClass(cardWrapper, "col-sm-4  h-50");
-          var cardBase = createItem("div");
-          addClass(cardBase, "card mb-3");
-          append(cardBase, cardWrapper);
-          var cardBody = createItem("div");
-          addClass(cardBody, "card-body");
-          append(cardBody, cardBase);
-          var cardTitle = createItem("h5");
-          addClass(cardTitle, "card-title");
-          addText(cardTitle, doc.data().title);
-          append(cardTitle, cardBody);
-          var cardText = createItem("p");
-          addClass(cardText, "card-text");
-          addText(cardText, doc.data().body);
-          append(cardText, cardBody);
-          var cardImage = createItem("img");
-          addClass(cardImage, "card-image-bottom");
-          if (doc.data().image != null) {
-            var imageRef = doc.data().image;
-            storage.refFromURL(imageRef).getDownloadURL().then((url) => {
-              addImage(cardImage, url, "Post Image");
-            });
-          }
-          append(cardImage, cardBase);
-          var cardFooterWrap = createItem("div");
-          addClass(cardFooterWrap, "card-footer");
-          append(cardFooterWrap, cardBase);
-          var cardFooter = createItem("p");
-          addClass(cardFooter, "card-text");
-          var timeNow = new Date().getTime();
-          var timeSince = Math.abs(Math.floor((timeNow - doc.data().addTime) / 60000));
-          var timeString = "";
-          if (timeSince >= 60) {
-            if ((timeSince / 60) >= 24) {
-              timeString = "Posted " + Math.floor((timeSince / 60) / 24) + " days ago";
+    var cardRoot = createItem("div");
+    var main = document.getElementById("main");
+    if (document.getElementsByClassName("card-group row").length === 0) {
+      addClass(cardRoot, "card-group row");
+      append(cardRoot, main);
+    } else {
+      document.getElementsByClassName("card-group row").item(0).remove();
+      addClass(cardRoot, "card-group row");
+      append(cardRoot, main);
+    }
+    querySnapshot.forEach((doc) => {
+      if (!doc.data().hide && !doc.metadata.hasPendingWrites) {
+        var cardWrapper = createItem("div");
+        addClass(cardWrapper, "col-sm-4  h-50");
+        var cardBase = createItem("div");
+        addClass(cardBase, "card mb-3");
+        append(cardBase, cardWrapper);
+        var cardBody = createItem("div");
+        addClass(cardBody, "card-body");
+        append(cardBody, cardBase);
+        var cardTitle = createItem("h5");
+        addClass(cardTitle, "card-title");
+        addText(cardTitle, doc.data().title);
+        append(cardTitle, cardBody);
+        var cardText = createItem("p");
+        addClass(cardText, "card-text");
+        addText(cardText, doc.data().body);
+        append(cardText, cardBody);
+        var cardImage = createItem("img");
+        addClass(cardImage, "card-image-bottom");
+        if (doc.data().image != null) {
+          var imageRef = doc.data().image;
+          storage.refFromURL(imageRef).getDownloadURL().then((url) => {
+            addImage(cardImage, url, "Post Image");
+          });
+        }
+        append(cardImage, cardBase);
+        var cardFooterWrap = createItem("div");
+        addClass(cardFooterWrap, "card-footer");
+        append(cardFooterWrap, cardBase);
+        var cardFooter = createItem("p");
+        addClass(cardFooter, "card-text");
+        var timeNow = new Date().getTime();
+        var timeSince = Math.abs(Math.floor((timeNow - doc.data().addTime) / 60000));
+        var timeString = "";
+        if (timeSince >= 60) {
+          if ((timeSince / 60) >= 24) {
+            if (Math.floor((timeSince / 60) / 24) = 1) {
+              timeString = "Posted " + Math.floor((timeSince / 60) / 24) + " day ago";
+            } else {
+              timeString = "Posted " + Math.floor((timeSince / 60)  / 24) + " days ago";
+            }
+          } else {
+            if (Math.floor(timeSince / 60) = 1) {
+              timeString = "Posted " + Math.floor(timeSince / 60) + " minute ago";
             } else {
               timeString = "Posted " + Math.floor(timeSince / 60) + " hours ago";
             }
+          }
+        } else {
+          if (timeSince = 1) {
+            timeString = "Posted " + timeSince + " minute ago";
           } else {
             timeString = "Posted " + timeSince + " minutes ago";
           }
-          addText(cardFooter, timeString);
-          append(cardFooter, cardFooterWrap);
-          if (cardRoot.childElementCount === 0) {
-            cardWrapper.id = "lastCard";
-            append(cardWrapper, cardRoot);
-          } else {
-            var lastCard = document.getElementById("lastCard");
-            insertCardBefore(cardWrapper, lastCard);
-            cardWrapper.id = lastCard.id;
-            lastCard.id = "";
-          }
         }
-      });
+        addText(cardFooter, timeString);
+        append(cardFooter, cardFooterWrap);
+        if (cardRoot.childElementCount === 0) {
+          cardWrapper.id = "lastCard";
+          append(cardWrapper, cardRoot);
+        } else {
+          var lastCard = document.getElementById("lastCard");
+          insertCardBefore(cardWrapper, lastCard);
+          cardWrapper.id = lastCard.id;
+          lastCard.id = "";
+        }
+      }
+    });
     //}, 2000);
   });
 }
